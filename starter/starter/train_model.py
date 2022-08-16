d2e853e985f1dacb54d70789409cd539a4bfb7da
Aragorn-X <diego.moglioni@gmail.com>
2022-08-16 17:46:34 +0200

    adding processing data, training model, inference and perfo

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -5,19 +5,20 @@ from sklearn.model_selection import train_test_split
 # Add the necessary imports for the starter code.
 import pandas as pd
 import os
-import csv
-import string
+from ml import data, model
+import pickle
+
 
 # Add code to load in the data.
 cur_dir = os.path.abspath(os.curdir)
 os.chdir("..")
 data_dir = 'data'
-csv_file = 'census_clean.csv'
+csv_file = 'census_clean.csv'    # cleaned file (all spaces removed)
 data_path = os.path.join(os.path.abspath(os.curdir), data_dir, csv_file)
-data = pd.read_csv(data_path)
-input('wait')
+data_df = pd.read_csv(data_path)
+
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data_df, test_size=0.20)
 
 cat_features = [
     "workclass",
@@ -29,10 +30,38 @@ cat_features = [
     "sex",
     "native-country",
 ]
-X_train, y_train, encoder, lb = process_data(
+
+
+X_train, y_train, encoder, lb = data.process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
+X_test, y_test, _, _ = data.process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
 
-# Train and save a model.
+# Train model
+trained_model = model.train_model(X_train, y_train)
+
+# Inference and performance metrics
+predictions = model.inference(trained_model, X_test)
+precision, recall, f_beta = model.compute_model_metrics(y_test, predictions)
+print('precision: ', precision)
+print('recall: ', recall)
+print('f_beta: ', f_beta)
+
+# Save model
+model_dir = 'model'
+model_path = os.path.join(os.path.abspath(os.curdir), model_dir, 'model.pkl')
+
+with open(model_path,'wb') as model_file:
+    pickle.dump(trained_model, model_file)
+
+
+""" 
+# load
+with open('model.pkl', 'rb') as f:
+    clf2 = pickle.load(f)
+"""
