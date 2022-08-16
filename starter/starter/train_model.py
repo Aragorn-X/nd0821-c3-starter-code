89db7248775a945227aa799ac29df84c0f1fb538
Aragorn-X <diego.moglioni@gmail.com>
2022-08-16 15:16:43 +0200

    reading clean census file

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -5,15 +5,17 @@ from sklearn.model_selection import train_test_split
 # Add the necessary imports for the starter code.
 import pandas as pd
 import os
+import csv
+import string
 
 # Add code to load in the data.
 cur_dir = os.path.abspath(os.curdir)
 os.chdir("..")
 data_dir = 'data'
-csv_file = 'census.csv'
-data_path = os.path.join(cur_dir, data_dir, csv_file)
+csv_file = 'census_clean.csv'
+data_path = os.path.join(os.path.abspath(os.curdir), data_dir, csv_file)
 data = pd.read_csv(data_path)
-
+input('wait')
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
