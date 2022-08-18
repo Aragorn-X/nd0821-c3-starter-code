5ae3e23b21a639ce8c19cc9529192a537dbf15e3
Aragorn-X <diego.moglioni@gmail.com>
2022-08-18 09:53:36 +0200

    code refactoring

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -10,54 +10,68 @@ import pickle
 
 
 # Add code to load in the data.
-cur_dir = os.path.abspath(os.curdir)
-os.chdir("..")
-data_dir = 'data'
-csv_file = 'census_clean.csv'    # cleaned file (all spaces removed)
-data_path = os.path.join(os.path.abspath(os.curdir), data_dir, csv_file)
-data_df = pd.read_csv(data_path)
-
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data_df, test_size=0.20)
-
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
+def import_data(path):
+    '''
+    :param path: path to csv file
+    :return: df: pandas dataframe
+    '''
+    df = pd.read_csv(path)
+    return df
 
 
-X_train, y_train, encoder, lb = data.process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
 
-# Process the test data with the process_data function.
-X_test, y_test, _, _ = data.process_data(
-    test, categorical_features=cat_features, label="salary", training=False,
-    encoder=encoder, lb=lb
-)
+if __name__ == "__main__":
+    cur_dir = os.path.abspath(os.curdir)
+    os.chdir("..")
+    data_dir = 'data'
+    csv_file = 'census_clean.csv'  # cleaned file (all spaces removed)
+    data_path = os.path.join(os.path.abspath(os.curdir), data_dir, csv_file)
+    data_df = import_data(data_path)
 
-# Train model
-trained_model = model.train_model(X_train, y_train)
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data_df, test_size=0.20)
 
-# Inference and performance metrics
-predictions = model.inference(trained_model, X_test)
-precision, recall, f_beta = model.compute_model_metrics(y_test, predictions)
-print('precision: ', precision)
-print('recall: ', recall)
-print('f_beta: ', f_beta)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
 
-# Save model
-model_dir = 'model'
-model_path = os.path.join(os.path.abspath(os.curdir), model_dir, 'model.pkl')
+    X_train, y_train, encoder, lb = data.process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
 
-with open(model_path,'wb') as model_file:
-    pickle.dump(trained_model, model_file)
+    # Process the test data with the process_data function.
+    X_test, y_test, _, _ = data.process_data(
+        test, categorical_features=cat_features, label="salary", training=False,
+        encoder=encoder, lb=lb
+    )
+
+    # Train model
+    trained_model = model.train_model(X_train, y_train)
+
+    # Inference and performance metrics
+    predictions = model.inference(trained_model, X_test)
+    precision, recall, f_beta = model.compute_model_metrics(y_test, predictions)
+    print('precision: ', precision)
+    print('recall: ', recall)
+    print('f_beta: ', f_beta)
+
+    # Save model
+    model_dir = 'model'
+    model_path = os.path.join(os.path.abspath(os.curdir), model_dir, 'model.pkl')
+
+    with open(model_path, 'wb') as model_file:
+        pickle.dump(trained_model, model_file)
+
+
+
+
 
 
 """ 
