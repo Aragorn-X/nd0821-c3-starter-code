bf6b6e1d905d08eafc2d015c7b6c46c79a73a2b0
Aragorn-X <diego.moglioni@gmail.com>
2022-08-18 12:41:27 +0200

    adding third unit testing code

--- starter/starter/test_model.py
+++ starter/starter/test_model.py
@@ -27,6 +27,10 @@ def test_import(import_data):
 
 
 def test_training(training_model):
+    '''
+    test training_model:
+    - verify if the training_model output has 'fit' method
+    '''
     # cur_dir = os.path.abspath(os.curdir)
     os.chdir("..")
     data_dir = 'data'
@@ -60,6 +64,44 @@ def test_training(training_model):
             "Testing train_models: The model doesn't appear to have the 'fit' method")
         raise err
 
+
+def test_saving(save_model):
+    cur_dir = os.path.abspath(os.curdir)
+    data_dir = 'data'
+    csv_file = 'census_clean.csv'  # cleaned file (all spaces removed)
+    data_path = os.path.join(cur_dir, data_dir, csv_file)
+    data_df = train_model.import_data(data_path)
+    train, test = train_test_split(data_df, test_size=0.20)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder, lb = data.process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+
+    output_model = train_model.training_model(X_train, y_train)
+
+    model_dir = 'model'
+    model_path = os.path.join(os.path.abspath(os.curdir), model_dir, 'model.pkl')
+
+    try:
+        save_model(model_path, output_model)
+        logging.info("Testing save_model: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing save_model: The file wasn't found")
+        raise err
+
+
 if __name__ == "__main__":
     test_import(train_model.import_data)
     test_training(train_model.training_model)
+    test_saving(train_model.save_model)
