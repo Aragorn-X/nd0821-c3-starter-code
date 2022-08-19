d8f6cc595446fc331688b88a40b2c1dc072b4041
Aragorn-X <diego.moglioni@gmail.com>
2022-08-19 17:32:22 +0200

    building FastAPI

--- starter/main.py
+++ starter/main.py
@@ -1,5 +1,6 @@
 # Put the code for your API here.
 from fastapi import FastAPI
+from input_data_definition import InputStructure
 
 # instantiating the app
 app = FastAPI()
@@ -8,3 +9,7 @@ app = FastAPI()
 @app.get("/")
 async def say_welcome():
     return {"greeting": "Welcome!"}
+
+@app.post("/")
+async def perform_inference(input_data: InputStructure):
+    return
