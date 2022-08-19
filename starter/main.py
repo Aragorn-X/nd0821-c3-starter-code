ef4460d9fd29e427152f11161435056486d0aaa7
Aragorn-X <diego.moglioni@gmail.com>
2022-08-19 10:18:55 +0200

    adding GET code

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,10 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+
+# instantiating the app
+app = FastAPI()
+
+# defining a GET on the specified endpoint
+@app.get("/")
+async def say_welcome():
+    return {"greeting": "Welcome!"}
