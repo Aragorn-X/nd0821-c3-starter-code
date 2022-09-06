42d3630827be8d13f39a1bccfebf8dc95223cac7
Aragorn-X <diego.moglioni@gmail.com>
2022-09-06 17:19:47 +0200

    refactoring

--- starter/starter/test_model.py
+++ starter/starter/test_model.py
@@ -2,7 +2,10 @@ import os
 import logging
 import pandas as pd
 #import train_model
-from ml import data, model
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+
+# from ml import data, model
 
 from train_model import import_data, training_model, save_model
 from sklearn.model_selection import train_test_split
@@ -13,6 +16,74 @@ logging.basicConfig(
     level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
+
+def process_data(
+    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
+):
+    """ Process the data used in the machine learning pipeline.
+
+    Processes the data using one hot encoding for the categorical features and a
+    label binarizer for the labels. This can be used in either training or
+    inference/validation.
+
+    Note: depending on the type of model used, you may want to add in functionality that
+    scales the continuous data.
+
+    Inputs
+    ------
+    X : pd.DataFrame
+        Dataframe containing the features and label. Columns in `categorical_features`
+    categorical_features: list[str]
+        List containing the names of the categorical features (default=[])
+    label : str
+        Name of the label column in `X`. If None, then an empty array will be returned
+        for y (default=None)
+    training : bool
+        Indicator if training mode or inference/validation mode.
+    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+        Trained sklearn OneHotEncoder, only used if training=False.
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained sklearn LabelBinarizer, only used if training=False.
+
+    Returns
+    -------
+    X : np.array
+        Processed data.
+    y : np.array
+        Processed labels if labeled=True, otherwise empty np.array.
+    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+        Trained OneHotEncoder if training is True, otherwise returns the encoder passed
+        in.
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained LabelBinarizer if training is True, otherwise returns the binarizer
+        passed in.
+    """
+
+    if label is not None:
+        y = X[label]
+        X = X.drop([label], axis=1)
+    else:
+        y = np.array([])
+
+    X_categorical = X[categorical_features].values
+    X_continuous = X.drop(*[categorical_features], axis=1)
+
+    if training is True:
+        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
+        lb = LabelBinarizer()
+        X_categorical = encoder.fit_transform(X_categorical)
+        y = lb.fit_transform(y.values).ravel()
+    else:
+        X_categorical = encoder.transform(X_categorical)
+        try:
+            y = lb.transform(y.values).ravel()
+        # Catch the case where y is None because we're doing inference.
+        except AttributeError:
+            pass
+
+    X = np.concatenate([X_continuous, X_categorical], axis=1)
+    return X, y, encoder, lb
+
 
 
 def test_import(import_data):
@@ -52,7 +123,7 @@ def test_training(training_model):
         "native-country",
     ]
 
-    X_train, y_train, encoder, lb = data.process_data(
+    X_train, y_train, encoder, lb = process_data(
         train, categorical_features=cat_features, label="salary", training=True
     )
 
@@ -86,7 +157,7 @@ def test_saving(save_model):
         "native-country",
     ]
 
-    X_train, y_train, encoder, lb = data.process_data(
+    X_train, y_train, encoder, lb = process_data(
         train, categorical_features=cat_features, label="salary", training=True
     )
 
