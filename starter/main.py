4a6942f1133abfda40807bcecea05b4d59a1eaac
Aragorn-X <diego.moglioni@gmail.com>
2022-09-07 23:57:29 +0200

    adding example

--- starter/main.py
+++ starter/main.py
@@ -32,6 +32,24 @@ class DataModel(BaseModel):
 
     class Config:
         allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "age": [21],
+                "workclass": ["Private"],
+                "fnlgt": [338400],
+                "education": ["Masters"],
+                "education-num": [10],
+                "marital-status": ["Married-AF-spouse"],
+                "occupation": ["Sales"],
+                "relationship": ["Other-relative"],
+                "race": ["White"],
+                "sex": ["Male"],
+                "capital-gain": [2100],
+                "capital-loss": [0],
+                "hours-per-week": [32],
+                "native-country": ["Scotland"]
+            }
+        }
 
 
 # Instantiating the app
